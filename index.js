bec67b9cdf80740385529846ae3c026de057468b
zy432099 <46698434+zy432099@users.noreply.github.com>
2024-12-19 15:36:25 +0800

    开源五项核查 (#12)
    Co-authored-by: 张宇 <478110507@qq.com>

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// Placeholder file so that Expo plugin module resolution works
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
 
 import { openInbox, openComposer, getEmailClients } from "./src/harmony";
 import { EmailException } from "./src/email-exception";
